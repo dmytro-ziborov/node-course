8c7d5ce523367255a893fb82eb1ebdfc9092b450
dmytro-ziborov <115806856+dmytro-ziborov@users.noreply.github.com>
2023-01-17 16:34:02 +0200

    added task 1.2.2 http-request

---
+++ 1.Basics/1.2.HTTP and web/HTTP request/go2.js
@@ -0,0 +1,43 @@
+// этот файл надо будет дописать...
+
+// не обращайте на эту функцию внимания 
+// она нужна для того чтобы правильно читать входные данные
+function readHttpLikeInput() {
+    var fs = require("fs");
+    var res = "";
+    var buffer = Buffer.alloc ? Buffer.alloc(1) : new Buffer(1);
+    let was10 = 0;
+    for (; ;) {
+        try { fs.readSync(0 /*stdin fd*/, buffer, 0, 1); } catch (e) { break; /* windows */ }
+        if (buffer[0] === 10 || buffer[0] === 13) {
+            if (was10 > 10)
+                break;
+            was10++;
+        } else
+            was10 = 0;
+        res += new String(buffer);
+    }
+
+    return res;
+}
+
+let contents = readHttpLikeInput();
+
+// вот эту функцию собственно надо написать
+
+function parseTcpStringAsHttpRequest(string) {
+    return {
+        method: string.match(/\w+/)[0],
+        uri: string.match(/\/(\w*\/?)*/)[0],
+        headers: string.match(/^.+:.+$/gm).reduce((accumulator, current) => {
+            let header = current.split(':');
+            header[0] = header[0].toLowerCase().replace(/(?=\b)[a-z]/gm, (word) => word[0].toUpperCase())
+            accumulator[header[0]] = header[1].trim();
+            return accumulator;
+        }, {}),
+        body: string.match(/^(&?.*=.*)+$/gm).join()
+    }
+}
+
+http = parseTcpStringAsHttpRequest(contents);
+console.log(JSON.stringify(http, undefined, 2));
