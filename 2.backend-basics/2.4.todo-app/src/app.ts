4759ab77bf42c1bf09f18f8f667727286b729925
dmytro-ziborov <115806856+dmytro-ziborov@users.noreply.github.com>
2023-02-20 17:46:48 +0200

    updated config and app.ts

--- 2.backend-basics/2.4.todo-app/src/app.ts
+++ 2.backend-basics/2.4.todo-app/src/app.ts
@@ -1,12 +1,25 @@
 import express, { Express, Request, Response } from "express";
 import logger from "./middleware/logger";
+import path from "path";
+import { MainRouter } from "./routes/index.routes";
+import bodyParser, { text } from "body-parser";
+import cookieParser from "cookie-parser";
+import { sessionOptions } from "./utils/config";
+import session from "express-session";
+import db from "./database/db";
 
 const app: Express = express();
 
-//todo add routes
-//simple console logger
-app.use(logger);
+app.use(express.static(path.join(__dirname, "public")));
 
-//todo add ...
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(session(sessionOptions));
+
+app.use(logger);
+app.use("/api", MainRouter);
+
+db.connect();
 
 export default app;
--- 2.backend-basics/2.4.todo-app/src/utils/config.ts
+++ 2.backend-basics/2.4.todo-app/src/utils/config.ts
@@ -1,6 +1,32 @@
 import dotenv from "dotenv";
+import session from "express-session";
+import fileStore, { FileStore } from "session-file-store";
 
 dotenv.config();
 
 export const APP_PORT = process.env.APP_PORT || 3000;
+
+export const DB_NAME = process.env.DB_NAME || "";
+export const DB_USERNAME = process.env.DB_USERNAME || "";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "";
+export const DB_HOST = process.env.DB_HOST || "";
+
+declare module "express-session" {
+  interface SessionData {
+    username: string;
+  }
+}
+
+const FileStore = fileStore(session);
+
+export const sessionOptions: session.SessionOptions = {
+  secret: process.env.SESSION_SECRET || "secret",
+  resave: false,
+  saveUninitialized: true,
+  store: new FileStore({}),
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24,
+  },
+};
+
 //todo add other
